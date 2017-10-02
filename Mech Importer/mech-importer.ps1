809994317c47b11108f607f5e0dcddfc5fca5542
Markemp <ggerber@gmail.com>
2017-10-01 18:50:04 -0700

    Armature import and parenting working.

--- Mech Importer/mech-importer.ps1
+++ Mech Importer/mech-importer.ps1
@@ -8,30 +8,48 @@
 # version 1.5 (6/12/14) - Modified to work with the new Clan mech files
 # version 1.6 (12/30/14) - Fixing bugs with Wave 2 mechs, variant/window materials assigned, general cleanup
 # Version 1.61 (8/9/15)  - Bug fixes (Blender 2.73+ crash, atlas_movie.cdf issue)
+# Version 2.0 (9/30/17) - Supports Collada files created by the 1.0 version of cgf-converter (https://www.heffaypresents.com/GitHub)
 
 # Input is the .cdf file in the mech directory for the mech you want
 # output is the text of what you want to put into Blender.  It also outputs to import.txt in the directory you
 # run the script from.
 
-# Input:  the directory to all the .obj files put in.
-
 function Get-Usage {
-    Write-host "Usage:  mech-importer <mech .cdf file>" -ForegroundColor Yellow
+    Write-host "Usage:  mech-importer <mech .cdf file> [-obj] [-objectdir `"<directory to \Object>`"]" -ForegroundColor Yellow
+	Write-Host "        Takes a mech's cdf file (in Objects\Mechs\<mech>) and creates an import.txt file that"
+	Write-Host "        can be pasted into the Blender python console.  This will import all the mech parts"
+	Write-Host "        (assuming they've been converted to .dae or .obj) into Blender and create the appropriate"
+	Write-Host "        materials.\n"
+	Write-Host
     Write-Host "        If .cdf file isn't specified, Mech Importer checks the current directory"
     Write-Host "        Please update the script before running.  The following variables need to be properly defined:"
     Write-Host "             `$basedir:  Where you extracted the object.pak files (and skins)"
     Write-Host "             `$imageformat:  What image format you are using (default .dds)"
-    Write-Host ""
-    Write-Host "        Please use Noesis with the cryengine plug-in to convert all the .cgf files to .obj with -flipUV first"
-    Write-Host "        See https://www.youtube.com/playlist?list=PL106ZeLhxxVn551_IKGKeU_LBODtkh29b for tutorials."
     Write-Host
     pause
     exit
 }
 
-$basedir = "e:\blender projects\mechs"  # this is where you extracted all the *.pak files from the game. \objects, \textures etc
+# Generic error checking
+if ($PSVersionTable.PSVersion.Major -lt 3) {
+	Write-Host "Requires at least Powershell version 3.  This computer is currently using version $PSVersionTable.PSVersion.Major" -ForegroundColor Yellow
+	exit 1
+}
+#
+
+# $type determines if you're using Collada or Waveform files.  Defaults to Collada.
+$type = "Collada"
+#$allArgs = $PSBoundParameters
+foreach ($arg in $args) {
+	if ($arg -eq "-obj") {
+		$type = "Waveform"
+	}
+}
+
+
+$basedir = "d:\blender projects\mechs"  # this is where you extracted all the *.pak files from the game. \objects, \textures etc
                                         # will be under this dir
-$imageformat = ".dds"                   # Default image file format.  If you want to use .pngs, change this
+$imageformat = ".dds"                   # Default image file format.  If you want to use .pngs, change this (although you probably don't want to.
 
 # convert the path so it can be used by Blender
 $basedir = $basedir.replace("\","\\")
@@ -41,6 +59,7 @@ $weapons = "hero","missile","narc","uac","ac2","ac5","ac10","ac20","gauss","ppc"
 
 # Python commands used by Blender
 $scriptimport = "bpy.ops.import_scene.obj"
+$scriptimportCollada = "bpy.ops.wm.collada_import"
 $scriptscene = "bpy.context.scene.objects.active"
 $scriptrotationmode = "bpy.context.active_object.rotation_mode=`"QUATERNION`""
 $scriptrotation = "bpy.context.active_object.rotation_quaternion"
@@ -69,7 +88,7 @@ if ( (get-childitem *.cdf)) {
     #$cdffilelocation = Get-childitem *.cdf   # Doesn't work if there is more than one .cdf file (looking at you Atlas)
 	foreach ($file in (Get-ChildItem *.cdf)) {
 		if (!$file.Name.Contains("movie")) {
-			$cdffilelocation = this;
+			$cdffilelocation = $file;
 		}
 	}
 } else {
@@ -83,6 +102,16 @@ if ( !(test-path $cdffilelocation)) {
 	exit
 }
 
+# Delete import.txt if it already exists.
+try {
+	$importtxt = Get-ChildItem "import.txt"
+	Remove-Item $importtxt
+}
+catch  {
+	# File not found.
+	Write-Host "No existing import.txt file found. (this is ok)"
+}
+
 # Get mech name from .cdf file name
 $mech = $cdffilelocation.directory.Name
 #$mech = $args[0].ToString().Substring(0,($args[0].tostring().Length-4))
@@ -91,13 +120,7 @@ if ($mech.startswith(".\")) {   # Need to strip off the .\
 }
 write-host "Mech is $mech"
 
-#function parent_bone parents $object to $bone  TBD
-function parent_bone($object, $bone) {
-}
-
 [xml]$cdffile = get-content $cdffilelocation
-
-
 
 # Get $modeldir from XML file.  Path to everything will be $basedir\$modeldir
 #$modeldir = $cdffile.Characterdefinition.Model.Material
@@ -117,6 +140,13 @@ Write-Host "Modeldir is $modeldir"
 
 # Set Blender to Cycles
 "bpy.context.scene.render.engine = 'CYCLES'" >> .\import.txt
+
+# If this is a Collada file, add the Armature
+if ($type -eq "Collada") {
+	$armature = $basedir + $modeldir + "\\body\\" + $mech  + ".dae"
+	Write-Host $armature
+	$scriptimportCollada + "(filepath=`"$armature`",find_chains=True,auto_connect=True)" >> .\import.txt
+}
 
 # Get the 4 materials from $matfile and create them in Blender
 #  material append wants an object of type material, not a string.  Will have to generate that.
@@ -178,8 +208,7 @@ $matfile.Material.SubMaterials.Material | % {
             "links.new(shaderRGBtoBW.outputs[0],shout.inputs[2])" >> .\import.txt
         }
     }
-    #$cTex = "bpy.data.materials.new('$matname')"
-    #$cTeximage = $matdiffuse
+	# Get each material into a material object.
 
 }
 
@@ -241,92 +270,134 @@ $matcockpitfile.Material.SubMaterials.Material | % {
     }
 }
 
-#  *** PARSING OBJs ***
+#  *** PARSING Files ***
 #  Start parsing out $mechline
 $cdffile.CharacterDefinition.attachmentlist.Attachment | % {
-    #TODO:  If you're reading a .cdf file here, it's recursive and you'll have to load in a few more files.  For now, just replace to .obj
-    #       for basic cockpit.
+	#TODO:  If you're reading a .cdf file here, it's recursive and you'll have to load in a few more files.  For now, just replace to .obj
+	#       for basic cockpit.
     
 	$aname = $_.AName
-    $rotation = $_.Rotation
-    $position = $_.Position
-    $bonename = $_.BoneName.replace(" ","_")
-    $binding = $_.Binding
-    $binding = $binding.replace(".cdf",".obj")
-    $binding = $binding.replace(".cga",".obj")
-    $binding = $binding.replace(".cgf",".obj")
-    $binding_modified = $binding.replace("\","\\").replace("/","\\")  # For use by script.  bug-fix for v1.6
-    #$binding_modified = $binding.replace("/","\\")  # For use by script
-    # Bug here.  If $binding_modified has just a regular \ as part of the path, Blender python console doesn't
-    # read it properly.  Replaces it with x08.  Need to replace single \ with \\.
-    $flags = $_.Flags
+	$rotation = $_.Rotation
+	$position = $_.Position
+	$bonename = $_.BoneName.replace(" ","_")
+	$binding = $_.Binding
+	if ($type -eq "Waveform") {
+		$binding = $binding.replace(".cdf",".obj")
+		$binding = $binding.replace(".cga",".obj")
+		$binding = $binding.replace(".cgf",".obj")
+	} else {
+		$binding = $binding.replace(".cdf",".dae")
+		$binding = $binding.replace(".cga",".dae")
+		$binding = $binding.replace(".cgf",".dae")
+	}
+	$binding_modified = $binding.replace("\","\\").replace("/","\\")  # For use by script.  bug-fix for v1.6
+	#$binding_modified = $binding.replace("/","\\")  # For use by script
+	# Bug here.  If $binding_modified has just a regular \ as part of the path, Blender python console doesn't
+	# read it properly.  Replaces it with x08.  Need to replace single \ with \\.
+	$flags = $_.Flags
 
-    # Figure out how to get the object name from $binding.
-    $objectname = $binding.split("/").split("\\")
-    $objectname = $objectname[-1]  #grabs last part of path
-    $objectname = $objectname.substring(0,$objectname.length-4) #strip last 4 characters 
+	# Figure out how to get the object name from $binding.
+	$objectname = $binding.split("/").split("\\")
+	$objectname = $objectname[-1]  #grabs last part of path
+	$objectname = $objectname.substring(0,$objectname.length-4) #strip last 4 characters 
 
-    # Assign the material based on the $objectname.  If it contains something in $weapon, assign the variant.
-    # Technically this is overly complex, as the cdf file doesn't accurately identify the material.  <mech>_body
-    # is assigned to basically everything even if it's a variant.
-    if ($_.material) {  # Material doesn't exist for every object.  Don't set it if it doesn't.
-        $material = $_.Material
-        $matname = $material.split("/").split("\")  # v1.6 bugfix:  Adds split("\") so it'll split off either way.
-        $matname = $matname[-1]
-        foreach ($weapon in $weapons) {
-            if ($aname.contains($weapon)) {  # We have a variant material
-                $matname = $mech+"_variant"
-                #write-host "variant found.  applying material $matname" -ForegroundColor Yellow
-            }
-        }
-        if ($objectname.EndsWith("head_cockpit")) { # Apply the window material
-            $matname = $mech+"_window"
-            #write-host "Window found.  Applying $matname" -ForegroundColor Red
-        }
+	# Assign the material based on the $objectname.  If it contains something in $weapon, assign the variant.
+	# Technically this is overly complex, as the cdf file doesn't accurately identify the material.  <mech>_body
+	# is assigned to basically everything even if it's a variant.
+	if ($_.material) {  # Material doesn't exist for every object.  Don't set it if it doesn't.
+		$material = $_.Material
+		$matname = $material.split("/").split("\")  # v1.6 bugfix:  Adds split("\") so it'll split off either way.
+		$matname = $matname[-1]
+		foreach ($weapon in $weapons) {
+			if ($aname.contains($weapon)) {  # We have a variant material
+				$matname = $mech+"_variant"
+				#write-host "variant found.  applying material $matname" -ForegroundColor Yellow
+			}
+		}
+		if ($objectname.EndsWith("head_cockpit")) { # Apply the window material
+			$matname = $mech+"_window"
+			#write-host "Window found.  Applying $matname" -ForegroundColor Red
+		}
        
-        $material = $material.replace("\","\\").replace("/","\\")  # v1.6 bugfix:  Adds replace("\","\\") so materials get assigned properly
-    }
-    #write-host "Matname for $objectname is $matname"
+		$material = $material.replace("\","\\").replace("/","\\")  # v1.6 bugfix:  Adds replace("\","\\") so materials get assigned properly
+	}
+	#write-host "Matname for $objectname is $matname"
 
-    # Time to generate the commands (in $parsedline, an array)
+	# Time to generate the commands (in $parsedline, an array)
 	$parsedline = @()
-    # if it's a cockpit item, it'll have multiple groups.  to avoid screwing up naming, we will import these keeping the vertex
-    # order with split_mode('OFF').  We do NOT want to remove doubles though, as this destroys the UVMap.
-    if ( $objectname.Contains("cockpit")) {
-        $parsedline += $scriptimport + "(filepath=`"$basedir\\$binding_modified`",use_groups_as_vgroups=True,split_mode=`'OFF`')" }
-    else {
-	    $parsedline += $scriptimport + "(filepath=`"$basedir\\$binding_modified`",use_groups_as_vgroups=True,split_mode=`'OFF`')" }
+	# if it's a cockpit item, it'll have multiple groups.  to avoid screwing up naming, we will import these keeping the vertex
+	# order with split_mode('OFF').  We do NOT want to remove doubles though, as this destroys the UVMap.
+	if ( $objectname.Contains("cockpit")) {
+		if ($type -eq "Waveform") {
+			$parsedline += $scriptimport + "(filepath=`"$basedir\\$binding_modified`",use_groups_as_vgroups=True,split_mode=`'OFF`')" 
+		} 
+		else {
+			$parsedline += $scriptimportCollada + "(filepath=`"$basedir\\$binding_modified`",find_chains=True,auto_connect=True)" 
+		}
+	}
+	else {
+		if ($type -eq "Waveform") {
+			$parsedline += $scriptimport + "(filepath=`"$basedir\\$binding_modified`",use_groups_as_vgroups=True,split_mode=`'OFF`')" 
+		} 
+		else {
+			$parsedline += $scriptimportCollada + "(filepath=`"$basedir\\$binding_modified`",find_chains=True,auto_connect=True)" 
+		}
+	}
     
-    # set new object as the active object
-    $parsedline += $scriptscene + "=bpy.data.objects[`"$objectname`"]"
-    # Parent the object to the Armature:  Assumes armature name is Armature and that it's been imported!
-    # $parsedline += $scriptscene + "=bpy.data.objects[`"Armature`"]"
-    # We may at this point (someday) to replace $objectname (above) with the $Aname, but for now let's stick with $objectname
-	$parsedline += $scriptrotationmode 
-	$parsedline += $scriptrotation + "=[$rotation]"
-	$parsedline += $scripttransform + "(value=($position))"
-	$parsedline += $scriptclearmaterial
-	$parsedline += $scriptseteditmode
-	# Check to see if it's a cockpit item, and if so don't remove doubles!
-    if ( !$objectname.Contains("cockpit")) {
-        $parsedline += $scriptremovedoubles }
+	# set new object as the active object
+	$parsedline += $scriptscene + "=bpy.data.objects[`"$objectname`"]"
+	# Parent the object to the Armature:  Assumes armature name is Armature and that it's been imported!
+	# $parsedline += $scriptscene + "=bpy.data.objects[`"Armature`"]"
+	# We may at this point (someday) to replace $objectname (above) with the $Aname, but for now let's stick with $objectname
+	# Only rotate/transform an item if it's not a proxy or begins with $.
+	if (!$objectname.Contains("proxy") -and !$objectname.StartsWith("$" )) {
+		$parsedline += $scriptrotationmode 
+		$parsedline += $scriptrotation + "=[$rotation]"
+		$parsedline += $scripttransform + "(value=($position))"
+		# $parsedline += $scripttristoquads
+		# Create a vertex group with the bone name.  This makes parenting to armature super easy!
+		$parsedline += $scriptseteditmode
+		$parsedline += "bpy.ops.object.vertex_group_add()"
+		$parsedline += "bpy.context.object.vertex_groups.active.name = `"$bonename`""
+		$parsedline += "bpy.ops.mesh.select_all(action=`'SELECT`')"
+		$parsedline += "bpy.ops.object.vertex_group_assign()"
+		$parsedline += "bpy.ops.mesh.select_all(action=`'TOGGLE`')"
 
-	$parsedline += $scripttristoquads
-    # Create a vertex group with the bone name.  This makes parenting to armature super easy!
-    $parsedline += "bpy.ops.object.vertex_group_add()"
-    $parsedline += "bpy.context.object.vertex_groups.active.name = `"$bonename`""
-    $parsedline += "bpy.ops.mesh.select_all(action=`'SELECT`')"
-    $parsedline += "bpy.ops.object.vertex_group_assign()"
-    $parsedline += "bpy.ops.mesh.select_all(action=`'TOGGLE`')"
-
-	$parsedline += $scriptsetobjectmode
-    $parsedline += "bpy.context.object.data.materials.append($matname)"
+		$parsedline += $scriptsetobjectmode
+		$parsedline += $scriptclearmaterial                     # Not sure if I want to do this or not.  Might wipe the BI materials from the .mtl file.
+		$parsedline += "bpy.context.object.data.materials.append($matname)"
+	}
 
 	foreach ( $line in $parsedline ) {
 		#write-host $line
 		$line >> .\import.txt
 	}
 }
+
+if ($type -eq "Collada") {
+	# Parent all the objects to the armature.
+			#	"armature = bpy.data.objects['Armature']
+			#objects =  bpy.context.selectable_objects
+			#objects.remove(armature)
+			#for x in range(1, len(objects)):
+			#	objects[x].parent = objects[0]
+			#" >> .\import.txt # Send additional line feed.
+	#(type='ARMATURE_NAME')
+	"bpy.ops.object.select_all(action='SELECT')
+bpy.context.scene.objects.active = bpy.data.objects['Armature']
+bpy.ops.object.parent_set(type='ARMATURE', xmirror=False, keep_transform=True)
+" >> .\import.txt
+}
+
+
+# Set material mode. # iterate through areas in current screen
+"for area in bpy.context.screen.areas:
+	if area.type == 'VIEW_3D':
+		for space in area.spaces: 
+			if space.type == 'VIEW_3D': 
+				space.viewport_shade = 'MATERIAL'
+				" >> .\import.txt
+
 "" >> .\import.txt # Send a final line feed.
 
 
